8cb1dc0614afbc1aca0f5bcb9eb69fe678c546dd
amna <ammy5342@gmail.com>
2025-07-29 15:51:31 +0300

    Modified grading script - Uses csv instead of tsv - Improved formatting of posted results - Better handling of paths

--- demo/test.sh
+++ demo/test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-return 0 # passing test
-#return 1 # failing test
+
+file_exists() {
+    [[ -f "$1" ]] || { echo "Required file not found: $1"; return 1; }
+}
+
+file_exists hi.txt # Passing test
+# file_exists none.txt # Failing test
+# exit 0 # blind passing test
+# exit 1 # blind failing test
+exit $?
