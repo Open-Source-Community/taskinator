5bf1576f9b7b35ee9448746b9640951c571748b4
amna <ammy5342@gmail.com>
2025-07-30 14:18:17 +0300

    Refactored task submission script

--- submit.sh
+++ submit.sh
@@ -1,61 +1,47 @@
 #!/bin/bash
 
-TASK_NAME=$1
-test_exist=0
+# Script to automatically upload task
+# This will submit the task in the folder it was executed in
 
-if [ $# == 0 ]; then
-	printf  "You shoud provide Task dir name -> ./submit TASK_dir_name\n"
-	exit 1
-fi
+test_file="test.sh"
 
-if [ ! -d $TASK_NAME ]; then
-	printf "INVALID dir name\nMake sure task dirctory is exist\n"
-	exit 1
-fi
-
-testing_task () {
-	echo "in function test"
-	cd $TASK_NAME
-	sumbit=0
-	if [ -f test.sh ]; then
-		./test.sh 
-		printf "\n"
-		if ! [ $? == 0 ]; then
-			echo "making submit false"
-			sumbit=1
-			printf "Error while running test file\n Do you want to submit? (y\n), default(n): "
-			read ch
-			if ! [ $ch == 'y' || $ch == 'Y' ]; then
-				submit=0
-			fi
+# Run the test script if provided with the task
+run_test() {
+	echo "Running test script..."
+	./"$test_file"
+	if [ $? != 0 ]; then
+		echo "Test failed. Do you still want to submit? (y\N)"
+		read -s -n1 choice
+		if ! [ $choice == 'y' ] || [ $choice == 'Y' ]; then
+			echo "Exiting without submitting task..."
+			exit 0
+		else
+			echo "Continuing..."
 		fi
-
-	fi
-	cd ..
-	if [ $sumbit == 1 ]; then
-		exit 1
 	fi
 }
 
-pushing_task () {
-	echo "in function publish"
-	pwd
-	if ! [ $(git branch --show-current) == 'master' -o $(git symbolic-ref --short HEAD) == 'main' ]; then
-		printf "Error: invalid branch, ensure you are in main or master branch"
+# Add, commit and push changes
+upload_task () {
+	local git_branch=$(git branch --show-current)
+	if [ "$git_branch" != 'master' ] && [ "$git_branch" != 'main' ]; then
+		echo "Error: Wrong branch! Ensure that you are on main or master."
 		exit 1
 	fi
+	if [ "$(git status -s | wc -l )" != 0 ]; then
+		git add . &> /dev/null 
+		git commit -m "Task submission." &> /dev/null 
+		git push &> /dev/null
+		echo "Task submitted successfully!"
 
-	if [ $(git status -s | wc -l ) != 0 ]; then
-		git add . > /dev/null 2>&1
-		git commit -m "commit changes" > /dev/null 2>&1
+	else
+		echo "No changes since last push."
 	fi
-
-	git push > /dev/null 2>&1
 	return 0
 }
 
-testing_task
-
-pushing_task
-
-echo "Your Task is Submitted successfully..:)!"
+# Main
+if [ -f "$test_file" ]; then
+	run_test
+fi
+upload_task
